1d719fef9294e093216480155e8c7bbca49fdb27
Nandana Mihindukulasooriya <nandana@ibm.com>
2024-07-07 19:25:23 -0400

    UP updates

--- src/app.py
+++ src/app.py
@@ -4,6 +4,8 @@ import pandas as pd
 
 def split_name_and_id(selected_id):
     if selected_id == "Not Selected":
+        del st.session_state["selected_name"]
+        del st.session_state["selected_dblp_id"]
         return None, None
     else:
         name, dblp_id = selected_id.split("(")
@@ -16,14 +18,15 @@ def set_current_view(c_view: str):
 
 def clear_dblp_id():
     st.session_state.current_view = "search_id"
-    st.session_state["selected_name"] = None
-    st.session_state["selected_dblp_id"] = None
+    del st.session_state["selected_name"]
+    del st.session_state["selected_dblp_id"]
 
 def on_selectbox_change():
     selected_value = st.session_state["selected_id"]
     name, dblp_id = split_name_and_id(selected_value)
-    st.session_state["selected_name"] = name
-    st.session_state["selected_dblp_id"] = dblp_id
+    if dblp_id:
+        st.session_state["selected_name"] = name
+        st.session_state["selected_dblp_id"] = dblp_id
 
 def view_search_id():
     st.subheader("Search for your DBLP ID")
@@ -36,6 +39,7 @@ def view_search_id():
         selected_results_container.subheader("Selected Person:")
         selected_results_container.write(f"\tFull Name: {st.session_state['selected_name']}")
         selected_results_container.write(f"\tDBLP ID: {st.session_state['selected_dblp_id']}")
+        selected_results_container.button("Clear selected person", on_click=clear_dblp_id, key="clear_id_0", type="primary")
 
     if search_button:
         res_list = get_person_candidates(author_name)
@@ -51,26 +55,35 @@ def view_search_id():
 
 
 def generate_coauthors_and_proceedings():
-    st.subheader("Generate coauthor and proceedings files for Wikidata linking")
-    # "https://dblp.org/pid/134/6661"
+    
+    
     if "selected_dblp_id" in st.session_state:
-        dblp_id = st.text_input("Your DBLP ID", key='dblp_id', value=st.session_state["selected_dblp_id"], disabled=True)
-        st.button("Clear DBLP ID", on_click=clear_dblp_id, key="clear_id_1")
+        sp1_container = st.container(border=True)
+        sp1_container.subheader("Selected Person:")
+        sp1_container.write(f"\tFull Name: {st.session_state['selected_name']}")
+        dblp_id = sp1_container.text_input("Your DBLP ID", key='dblp_id', value=st.session_state["selected_dblp_id"], disabled=True)
+        sp1_container.button("Clear selected person", on_click=clear_dblp_id, key="clear_id_1", type="primary")
     else:
         dblp_id = st.text_input("Your DBLP ID", key='dblp_id')
-    generate_coauthors_button = st.button('Generate Files')
-    if generate_coauthors_button:
-        print("Querying for coauthors in DBLP")
+
+    st.subheader("Generate coauthor and proceedings files for Wikidata linking")
+    generate_coauthors_button = st.button('Query DBLP and Generate Files')
+    if generate_coauthors_button or 'coauthor_list' in st.session_state:
+        #print("Querying for coauthors in DBLP")
         progress_bar = st.progress(33)
         status_text = st.empty()
         status_text.text(f"Progress: 33%")
-        coauthor_list = get_coauthors_list(dblp_id)
-        st.write(f"A CSV file is generated with {len(coauthor_list)} coauthors ...")
+        if 'coauthor_list' in st.session_state:
+            coauthor_list = st.session_state['coauthor_list']
+        else:
+            coauthor_list = get_coauthors_list(dblp_id)
+            st.session_state['coauthor_list'] = coauthor_list
+        st.subheader(f"Disticnt coauthors list ({len(coauthor_list)})")
         csv = coauthor_list.to_csv(index=False)
         csv_bytes = csv.encode('utf-8')
         st.dataframe(coauthor_list)
         st.download_button(
-            label="Download data as CSV",
+            label="Download coauthor list as a CSV",
             data=csv_bytes,
             file_name='coauthor_list.csv',
             mime='text/csv'
@@ -78,13 +91,17 @@ def generate_coauthors_and_proceedings():
         progress_bar.progress(66)
         status_text.text(f"Progress: 66%")
 
-        proceedings_list = get_proceedings_list(dblp_id)
-        st.write(f"A CSV file is generated with {len(proceedings_list)} proceedings ...")
+        if 'proceedings_list' in st.session_state:
+            proceedings_list = st.session_state['proceedings_list']
+        else:
+            proceedings_list = get_proceedings_list(dblp_id)
+            st.session_state['proceedings_list'] = proceedings_list
+        st.subheader(f"Disticnt proceedings list ({len(proceedings_list)})")
         pro_csv = proceedings_list.to_csv(index=False)
         pro_csv_bytes = pro_csv.encode('utf-8')
         st.dataframe(proceedings_list)
         st.download_button(
-            label="Download proceedings list as CSV",
+            label="Download proceedings list as a CSV",
             data=pro_csv_bytes,
             file_name='proceedings_list.csv',
             mime='text/csv',
@@ -93,38 +110,51 @@ def generate_coauthors_and_proceedings():
         status_text.text(f"Progress: 100%")
 
 def generate_articles_and_authors():
-    st.subheader("Generate scholary article list for ingesting to Wikidata")
+
     if "selected_dblp_id" in st.session_state:
-        dblp_id = st.text_input("Your DBLP ID", key='dblp_id_2', value=st.session_state["selected_dblp_id"], disabled=True)
-        st.button("Clear DBLP ID", on_click=clear_dblp_id, key="clear_id_2")
+        sp2_container = st.container(border=True)
+        sp2_container.subheader("Selected Person:")
+        sp2_container.write(f"\tFull Name: {st.session_state['selected_name']}")
+        dblp_id = sp2_container.text_input("DBLP ID", key='dblp_id', value=st.session_state["selected_dblp_id"], disabled=True)
+        sp2_container.button("Clear selected person", on_click=clear_dblp_id, key="clear_id_2", type="primary")
     else:
-        dblp_id = st.text_input("Your DBLP ID", key='dblp_id_2')
-    st.subheader("Linked Coauthors File:")
-    coauthor_map_file = st.file_uploader("Upload coauthor map file", type="csv")
+        sp2_container = st.container(border=True)
+        dblp_id = sp2_container.text_input("Your DBLP ID", key='dblp_id')
+
+    
+    upload_container = st.container(border=True)
+    upload_container.subheader("Upload mapping files from OpenRefine")
+    coauthor_map_file = upload_container.file_uploader("Upload coauthor map file", type="csv")
     if coauthor_map_file is not None:
         coauthor_map_df = pd.read_csv(coauthor_map_file)
         coauthor_map_df = coauthor_map_df[['dblp_id', 'name', 'wd_id']]
         coauthor_map_df = coauthor_map_df[coauthor_map_df['wd_id'].notna() & (coauthor_map_df['wd_id'] != '')]
-        st.write(coauthor_map_df)
+        upload_container.write(coauthor_map_df)
         author_map_dict = coauthor_map_df.set_index('name')['wd_id'].to_dict()
 
-    proceedings_map_file = st.file_uploader("Upload proceedings map file", type="csv")
+    proceedings_map_file = upload_container.file_uploader("Upload proceedings map file", type="csv")
     if proceedings_map_file is not None:
         proceedings_map_df = pd.read_csv(proceedings_map_file)
         proceedings_map_df = proceedings_map_df[proceedings_map_df['wd_id'].notna() & (proceedings_map_df['wd_id'] != '')]
         proceedings_map_df = proceedings_map_df[['title', 'dblp_id', 'wd_id']]
-        st.write(proceedings_map_df)
+        upload_container.write(proceedings_map_df)
         proceedings_map_dict = proceedings_map_df.set_index('dblp_id')['wd_id'].to_dict()
 
-    #df_dict = proceedings_map_df.set_index('proceedings')['name'].to_dict()
+    st.subheader("Generate scholary article list for ingesting to Wikidata")
 
-    dblp_id = "https://dblp.org/pid/134/6661"
-    generate_articles_button = st.button('Generate Files')
+    generate_articles_button = st.button('Generate files')
     if generate_articles_button:
+        #print("Querying for scholarly articles in DBLP")
+        progress_bar_2 = st.progress(33)
+        status_text_2 = st.empty()
+        status_text_2.text(f"Progress: 33%")
         scholarly_article_list = get_scholarly_article_list(dblp_id)
         if proceedings_map_dict:
             scholarly_article_list['proceedings_id'] = scholarly_article_list['proceedings_id'].map(proceedings_map_dict)
         st.dataframe(scholarly_article_list)
+
+        progress_bar_2.progress(66)
+        status_text_2.text(f"Progress: 66%")
 
         scholarly_article_author_list = get_scholarly_article_author_list(dblp_id)
         if author_map_dict:
@@ -134,13 +164,17 @@ def generate_articles_and_authors():
         scholarly_article_author_list = scholarly_article_author_list[['dblp_id', 'title', 'ordinal', 'author_wd_id', 'author_name_string']]
         st.dataframe(scholarly_article_author_list)
 
+        progress_bar_2.progress(100)
+        status_text_2.text(f"Progress: 100%")
+
 
 # Initialize session state if not already done
 if 'current_view' not in st.session_state:
     st.session_state.current_view = "search_id"
 
-st.title('DBLP to Wikidata: Publish Your Scholar Data to Wikidata')
-st.sidebar.title("Follow the steps:")
+st.title('DBLP to Wikidata: Populating Scholarly Articles in Wikidata')
+st.sidebar.button("About")
+st.sidebar.title("Follow these steps:")
 st.sidebar.button("1. Search your DBLP ID", on_click=set_current_view, args=('search_id',))
 st.sidebar.button("2. Generate coauthors and proceedings", on_click=set_current_view, args=('coauthor_proceeding',))
 st.sidebar.button("3. Generate scholarly articles and article authors", on_click=set_current_view, args=('articles_authors',))
